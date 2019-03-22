83f21e751f11cb211ecbf195c3fc6bf6836a20f8
Ondel Lucas <iondel@fit.vutbr.cz>
2019-03-22 16:24:59 +0100

    Fixed script when egg archived is zipped.

--- setup.py
+++ setup.py
@@ -14,10 +14,11 @@ setup(
     version=1.0,
     packages=['mbnextractor'],
     scripts=['mbnextractor/audio2bottleneck'],
-    data_files=[
-        ('mbnextractor/models', ['mbnextractor/models/FisherEnglish_FBANK_HL500_SBN80_PhnStates120.npz']),
-        ('mbnextractor/models', ['mbnextractor/models/FisherEnglish_SBN80_triphones2423.npz']),
-        ('mbnextractor/models', ['mbnextractor/models/Babel-ML17_SBN80_PhnStates3096.npz']),
-    ]
+    package_data = {'mbnextractor': ['models/*.npz']},
+    #data_files=[
+    #    ('models', ['mbnextractor/models/FisherEnglish_FBANK_HL500_SBN80_PhnStates120.npz']),
+    #    ('models', ['mbnextractor/models/FisherEnglish_SBN80_triphones2423.npz']),
+    #    ('models', ['mbnextractor/models/Babel-ML17_SBN80_PhnStates3096.npz']),
+    #]
 )
 
